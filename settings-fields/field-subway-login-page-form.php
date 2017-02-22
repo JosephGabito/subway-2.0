b5e5ba99a21dda7b8947a6ebc471bad88be592c3
Joseph Gabito <codehaiku.io@gmail.com>
2017-02-22 14:00:46 +0800

    Added file header

--- settings-fields/field-subway-login-page-form.php
+++ settings-fields/field-subway-login-page-form.php
@@ -1,5 +1,20 @@
 <?php
 /**
+ * This file is part of the Subway WordPress Plugin Package.
+ *
+ * (c) Joseph Gabito <joseph@useissuestabinstead.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Subway
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	return;
+}
+
+/**
  * Callback function for 'subway_login_page' setting
  *
  * @return void
