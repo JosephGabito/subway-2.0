06d4f93964372ace5c94f0836365ae68c4a9c916
Joseph Gabito <codehaiku.io@gmail.com>
2017-02-22 14:00:49 +0800

    Added file header

--- templates/login-form.php
+++ templates/login-form.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * This file is part of the Subway WordPress Plugin Package.
+ *
+ * (c) Joseph Gabito <joseph@useissuestabinstead.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Subway
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	return;
+}
+
 $args = array(
 	'echo'           => true,
 	'form_id'        => 'loginform',
