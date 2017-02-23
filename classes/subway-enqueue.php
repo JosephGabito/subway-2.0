c59896a3e5bccf8c4f543072119db4b025637bc7
Joseph Gabito <codehaiku.io@gmail.com>
2017-02-23 12:08:51 +0800

    Fixed Enqueue

--- classes/subway-enqueue.php
+++ classes/subway-enqueue.php
@@ -9,28 +9,51 @@
  *
  * @package Subway
  */
+
 namespace Subway;
 
 if ( ! defined( 'ABSPATH' ) ) {
-   return;
+	return;
 }
 
+/**
+ * Subway\Enqueue class handles all the stylesheet and javascript documents for Subway
+ *
+ * @since  2.0
+ */
 final class Enqueue {
-	
+
+	/**
+	 * Registers our CSS and Javascript to WordPress Enqueue Handler.
+	 *
+	 * @return void
+	 */
 	public static function register_js() {
 
-   		wp_enqueue_style( 'subway-style', SUBWAY_DIR_URL . 'assets/css/subway.css' );
+		$post_id = absint( get_queried_object_id() );
 
-   		wp_enqueue_script( 'subway-script', SUBWAY_DIR_URL . 'assets/js/subway.js', array('jquery') );
+		$signin_page = absint( get_option( 'subway_login_page' ) );
 
-   		wp_localize_script( 'subway-script', 'subway_config', array( 
-   				'ajax_url' => admin_url( 'admin-ajax.php' ),
-   				'login_http_error' => esc_html__( 'An error occured while transmitting the data. Refresh the page and try again', 'subway' )
-   			)
-   		);
+		// Only load the stylesheet and javascript documents inside our sign-in page.
+		if ( $post_id === $signin_page ) {
 
-   		return;
-   		
+			wp_enqueue_style( 'subway-style', SUBWAY_DIR_URL . 'assets/css/subway.css' );
+
+			if ( ! is_user_logged_in() ) {
+
+	   			wp_enqueue_script( 'subway-script', SUBWAY_DIR_URL . 'assets/js/subway.js', array( 'jquery' ) );
+
+	   			wp_localize_script( 'subway-script', 'subway_config', array(
+	   					'ajax_url' => admin_url( 'admin-ajax.php' ),
+	   					'login_http_error' => esc_html__( 'An error occured while transmitting the data. Refresh the page and try again', 'subway' ),
+	   				)
+	   			);
+
+			}
+		}
+
+		return;
+
 	}
 
 }
