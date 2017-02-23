4689201266bfe154de2324e3987d8fe95ab3e3d1
Joseph Gabito <codehaiku.io@gmail.com>
2017-02-23 12:08:51 +0800

    Added settings

--- settings-fields/field-subway-lock-wp-admin.php
+++ settings-fields/field-subway-lock-wp-admin.php
@@ -16,18 +16,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function subway_lock_wp_admin() { ?>
 
-	<label for="subway_redirect_wp_admin">
-		<input <?php echo checked( 1, get_option( 'subway_redirect_wp_admin' ), false ) ;?> value="1" name="subway_redirect_wp_admin" 
-		id="subway_redirect_wp_admin" type="checkbox" class="code" /> 
-		<?php esc_html_e( 'Check to hide /wp-admin or /wp-login.php to logged out users and redirect them to Subway custom login form.', 'subway' ); ?>
-		
-	</label>
-
 	<p class="description">
 		<?php 
-			echo sprintf( __( "<br/>In case, you were locked out. Use the link below to bypass the log-in page and go directly 
-			to your website's wp-login URL (http://yoursiteurl.com/wp-login.php):
-			<br> <strong>%s</strong>", 'subway' ), site_url( 'wp-login.php?no_redirect=true' ) ); 
+			echo sprintf( __( "In case, you were locked out. Use the link below to bypass the log-in page and go directly 
+			to your website's wp-login URL (http://yoursiteurl.com/wp-login.php): <strong style='color: #e53935;'>%s</strong>", 'subway' ), site_url( 'wp-login.php?no_redirect=true' ) ); 
 		?>
 	</p>
 <?php
