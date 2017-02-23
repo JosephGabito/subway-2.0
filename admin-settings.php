f5eaf9c68f59d55d034e062b600333886dec3da1
Joseph Gabito <codehaiku.io@gmail.com>
2017-02-23 15:42:19 +0800

    SensioLabs Insights and Scrutinizer Clean Up

--- admin-settings.php
+++ admin-settings.php
@@ -185,14 +185,14 @@ final class AdminSettings
         ?>
 
         <div class="wrap">
-         <h2>
-        <?php esc_html_e('Subway Settings', 'subway'); ?>
-         </h2>
-         <form id="subway-settings-form" action="options.php" method="POST">
-        <?php settings_fields('subway-settings-group'); ?>
-        <?php do_settings_sections('subway-settings-section'); ?>
-        <?php submit_button(); ?>
-         </form>
+            <h2>
+                <?php esc_html_e('Subway Settings', 'subway'); ?>
+             </h2>
+             <form id="subway-settings-form" action="options.php" method="POST">
+                <?php settings_fields('subway-settings-group'); ?>
+                <?php do_settings_sections('subway-settings-section'); ?>
+                <?php submit_button(); ?>
+             </form>
         </div>
         
         <?php
--- templates/login-form-lost-password.php
+++ templates/login-form-lost-password.php
@@ -6,8 +6,15 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * @package Subway
+ * 
+ * PHP Version 5.4
+ * 
+ * @category Subway\Templates
+ * @package  Subway
+ * @author   Joseph G. <emailnotdisplayed@domain.tld>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version  GIT:github.com/codehaiku/subway
+ * @link     github.com/codehaiku/subway The Plugin Repository
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
