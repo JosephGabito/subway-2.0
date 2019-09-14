41548db52fd79866c6875e8e97143f708b479485
Joseph Gabito <codehaiku.io@gmail.com>
2019-09-15 00:00:23 +0800

    added message settings for redirect and partial content

---
+++ settings-fields/field-subway-redirected-message-login-form.php
@@ -0,0 +1,14 @@
+<?php
+function subway_redirected_message_login_form() { ?>
+	
+	<?php $content = get_option('subway_redirected_message_login_form',
+		esc_html__('Members only page. Please use the login form below to access the page.','subway'));  ; ?>
+
+	<?php $settings = apply_filters('subway_redirected_message_login_form_editor',
+		array( 'teeny' => true, 'media_buttons' => true )); ?>
+
+	<?php echo wp_editor( $content, 'subway_redirected_message_login_form', $settings ); ?>
+	
+	<?php
+	return;
+}
