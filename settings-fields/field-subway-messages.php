2d5afaccf25119ddb12693b69ef5df1ab07a8eff
Joseph Gabito <codehaiku.io@gmail.com>
2019-09-20 08:20:54 +0800

    added doc block

--- settings-fields/field-subway-messages.php
+++ settings-fields/field-subway-messages.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * This file is part of the Subway WordPress Plugin Package.
+ *
+ * (c) Joseph Gabito <joseph@useissuestabinstead.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * PHP Version 5.4
+ * 
+ * @category Subway\SettingsFields
+ * @package  Subway
+ * @author   Joseph G. <emailnotdisplayed@domain.tld>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version  GIT:github.com/codehaiku/subway
+ * @link     github.com/codehaiku/subway The Plugin Repository
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	return;
+}
+
 function subway_messages(){
 	?>
 	<?php $content = $message = get_option('subway_partial_message',esc_html__('Please login to see this content','subway'));  ; ?>
